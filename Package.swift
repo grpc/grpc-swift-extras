6344b2020d25d52e1a72393979ed228749a0efdf
George Barnett <gbarnett@apple.com>
2024-10-01 15:12:46 +0100

    Set grpc-swift version to 2.0.0-alpha.1 (#5)
    Motivation:
    
    We just tagged grpc-swift 2.0.0-alpha.1, we should use it.
    
    Modifications:
    
    Update version requirement.
    
    Result:
    
    Better version management.

--- Package.swift
+++ Package.swift
@@ -35,11 +35,11 @@ let products: [Product] = [
 let dependencies: [Package.Dependency] = [
   .package(
     url: "https://github.com/grpc/grpc-swift.git",
-    branch: "main"
+    exact: "2.0.0-alpha.1"
   ),
   .package(
     url: "https://github.com/grpc/grpc-swift-protobuf.git",
-    branch: "main"
+    exact: "1.0.0-alpha.1"
   ),
   .package(
     url: "https://github.com/apple/swift-protobuf.git",
