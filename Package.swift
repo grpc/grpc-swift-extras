2795556eaba15fde694fe3a481e3a1be777616c2
George Barnett <gbarnett@apple.com>
2025-01-24 15:59:22 +0000

    Bump dependency versions

--- Package.swift
+++ Package.swift
@@ -43,11 +43,11 @@ let products: [Product] = [
 let dependencies: [Package.Dependency] = [
   .package(
     url: "https://github.com/grpc/grpc-swift.git",
-    branch: "main"
+    exact: "2.0.0-rc.1"
   ),
   .package(
     url: "https://github.com/grpc/grpc-swift-protobuf.git",
-    branch: "main"
+    exact: "1.0.0-rc.1"
   ),
   .package(
     url: "https://github.com/apple/swift-protobuf.git",
